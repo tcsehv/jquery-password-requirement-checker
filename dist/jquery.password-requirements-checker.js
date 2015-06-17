de8b6fa86134951ed618379bab684fe36fc9761e
Tijs van Erp <t.vanerp@theconceptstore.nl>
2015-06-17 12:37:32 +0200

    fixed a bug with amounts

--- dist/jquery.password-requirements-checker.js
+++ dist/jquery.password-requirements-checker.js
@@ -58,7 +58,6 @@
             $this.prepareAmounts();
             $this.runValidations(element);
 
-
             element.keyup(function () {
                 $this.runValidations(element);
             });
@@ -69,7 +68,7 @@
             var $this = this;
             var validations = {};
 
-            validations.length = ($this.validateLength(fieldValue));
+            validations.length = $this.validateLength(fieldValue);
 
             $.each($this.getRegexList(), function (key, value) {
                 validations[key] = $this.validateRegex(key, value, fieldValue);
@@ -124,7 +123,7 @@
             });
         },
         writeAmount: function(key, value) {
-            $("." + this.getValidationClass(key) + " " + this.getValidationClass("amount")).html(value);
+            $("." + this.getValidationClass(key) + " ." + this.getValidationClass("amount")).html(value);
         }
     });
 
